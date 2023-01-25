d747cd02f07192b020d324035026f01faabbdcde
Willian Sant' Anna <w.s.santanna@gmail.com>
2023-01-25 12:39:58 -0300

    Adcionei mais um exemplo de callback na Aula 26

--- 26/ao-vivo/1_callback.js
+++ 26/ao-vivo/1_callback.js
@@ -59,3 +59,77 @@ function adicionarPontoBr(url) {
 console.log('Adiciona http://',modificarUrls(['google.com', 'facebook.com', 'google.com'], adicionarHttp));
 console.log('Adiciona https://', modificarUrls(['google.com', 'facebook.com', 'google.com'], adicionarHttps));
 console.log('Adiciona https:// + .br', modificarUrls(['google.com', 'facebook.com', 'google.com'], adicionarPontoBr));
+
+// Exemplos EXTRA:
+
+// sem callback 
+
+function somarEmReais(valor1, valor2) {
+    return "R$ " + valor1 + valor2;
+}
+
+function subtrairReais(valor1, valor2) {
+    return "R$ " + valor1 - valor2;
+}
+
+function multiplicarEmReais(valor1, valor2) {
+    return "R$ " + valor1 * valor2;
+}
+
+function dividirReais(valor1, valor2) {
+    return "R$ " + valor1 / valor2;
+}
+
+function somarEmDolares(valor1, valor2) {
+    return "$ " + valor1 + valor2;
+}
+
+function subtrairEmDolares(valor1, valor2) {
+    return "$ " + valor1 - valor2;
+}
+
+function multiplicarEmDolares(valor1, valor2) {
+    return "$ " + valor1 * valor2;
+}
+
+function dividirEmDolares(valor1, valor2) {
+    return "$ " + valor1 / valor2;
+}
+
+// com callback
+
+function somar(valor1, valor2) {
+    return valor1 + valor2;
+}
+
+function subtrair(valor1, valor2) {
+    return valor1 - valor2;
+}
+
+function multiplicar(valor1, valor2) {
+    return valor1 * valor2;
+}
+
+function dividir(valor1, valor2) {
+    return valor1 / valor2;
+}
+
+function calcularEmReais(valor1, valor2, callbackDaOperacaoMatematica) {
+    return "R$ " + callbackDaOperacaoMatematica(valor1, valor2);
+}
+
+function calcularEmDolar(valor1, valor2, callbackDaOperacaoMatematica) {
+    return "$ " + callbackDaOperacaoMatematica(valor1, valor2);
+}
+
+// 
+calcularEmReais(2,2, somar); // R$ 4
+calcularEmReais(2,2, subtrair); // R$ 0
+calcularEmReais(2,2, multiplicar); // R$ 4
+calcularEmReais(2,2, dividir); // R$ 1
+
+// 
+calcularEmDolar(2,2, somar); // $ 4
+calcularEmDolar(2,2, subtrair); // $ 0
+calcularEmDolar(2,2, multiplicar); // $ 4
+calcularEmDolar(2,2, dividir); // $ 1
